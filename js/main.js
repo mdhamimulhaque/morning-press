bee96c72087bd5a3f8a42593fb2c8dc70028703c
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-09-03 01:02:42 +0600

    single category data load

--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@
 const loadCategory = async () => {
     try {
         const res = await fetch(`https://openapi.programming-hero.com/api/news/categories`)
-        const data = res.json()
-            .then(data => showCategories(data.data.news_category))
+        const data = await res.json()
+            .then(data => showCategories(data?.data?.news_category))
     } catch (err) {
         console.log(err)
     }
 }
+loadCategory()
 
 const categoriesWrapper = document.getElementById('category_wrapper');
 
@@ -18,10 +19,21 @@ const showCategories = (categories) => {
         const categoryItem = document.createElement('li');
         categoryItem.classList.add('nav-item', 'mx-3')
         categoryItem.innerHTML = `
-        <a class="nav-link hover_text" href="#">${category.category_name}</a>
+        <a onclick='singleCategoryDataLoad(${category?.category_id})' class="nav-link hover_text" href="#">${category?.category_name}</a>
         `;
         categoriesWrapper.appendChild(categoryItem);
     })
 
 }
-loadCategory()
+
+//! ========== single category's data load==========
+const singleCategoryDataLoad = async (id) => {
+    // console.log(id)
+    try {
+        const res = await fetch(`https://openapi.programming-hero.com/api/news/category/0${id}`)
+        const data = await res.json()
+        console.log(data)
+    } catch (err) {
+        console.log(err)
+    }
+}
